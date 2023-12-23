7267bb807435631edd2307e93a0e3590e571066d
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2023-12-24 01:51:00 +0530

    Added restaurant cards

--- app.js
+++ app.js
@@ -19,11 +19,32 @@ const Header = () => {
     )
 }
 
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/durpa8rlz2innph3xwgh"></img>
+            <h3 className="res-name">Barbeque Nation</h3>
+            <h4 className="cuisine">North Indian, Barbecue</h4>
+            <div className="rating">
+                <img className="star" src="https://www.freeiconspng.com/thumbs/stars-png/download-png-image-star-png-image-1.png"></img>
+                <h4>4.2</h4>
+                <h4 className="time">•   22 mins</h4>
+            </div>
+        </div>
+    )
+}
+
 const Body = () => {
     return (
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
             </div>
         </div>
     )
@@ -33,7 +54,7 @@ const AppLayout = () => {
     return (
         <div class="app">
             <Header />
-            {/* <Body /> */}
+            <Body /> 
         </div>
     )
 }
