1082e5533aae60210d124a61d8102a81ab098792
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2023-12-25 20:41:13 +0530

    Used  props

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ const Header = () => {
     )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
     return (
         <div className="res-card">
-            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/durpa8rlz2innph3xwgh"></img>
-            <h3 className="res-name">Barbeque Nation</h3>
-            <h4 className="cuisine">North Indian, Barbecue</h4>
+            <img className="res-logo" src={props.resLogo}></img>
+            <h3 className="res-name">{props.resName}</h3>
+            <h4 className="cuisine">{props.cuisine}</h4>
             <div className="rating">
                 <img className="star" src="https://www.freeiconspng.com/thumbs/stars-png/download-png-image-star-png-image-1.png"></img>
-                <h4>4.2</h4>
-                <h4 className="time">•   22 mins</h4>
+                <h4>{props.star}</h4>
+                <h4 className="time">• {props.time}</h4>
             </div>
         </div>
     )
@@ -39,12 +39,20 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                <RestaurantCard
+                    resLogo = "https://b.zmtcdn.com/data/pictures/chains/6/18694246/ce89a2b9888ae02bc47cb3643bfd5738.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+                    resName = "Barbeque Nation"
+                    cuisine = "North Indian, Barbecue"
+                    star = "4.2"
+                    time = "22 mins"
+                />
+                <RestaurantCard
+                    resLogo = "https://b.zmtcdn.com/data/pictures/chains/3/18694083/8c49ba335683507ba6ddee2dc84966e8_o2_featured_v2.jpg?output-format=webp"
+                    resName = "Burger King"
+                    cuisine = "Burgers, American"
+                    star = "4.3"
+                    time = "21 mins"
+                />
             </div>
         </div>
     )
