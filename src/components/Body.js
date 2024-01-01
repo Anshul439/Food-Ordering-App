140ca5e75a1ff5cd653db41535bf3d2494629e29
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-01-01 21:21:02 +0530

    Updated data retrieval using useState and fetch for Swiggy API integration

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,37 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 const Body = () => {
+
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3076824&lng=75.5875939&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+
+    const json = await data.json();
+  }
+
   return (
     <div className="body">
       <div className="filter">
         <button className="filter-btn" 
         onClick={() => {
-          console.log("Lol");
-        }}>Top Rated Restaurants</button>
+          const filteredList = listOfRestaurants.filter(
+            (res) => res.info.avgRating>4.3
+          );
+          setListOfRestaurants(filteredList); 
+        }}
+        >
+          Top Rated Restaurants</button>
       </div>
       <div className="res-container">
         {
-          resList.map((restaurant) => (
+          listOfRestaurants.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))}
       </div>
