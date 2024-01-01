afa5cbcdd47e7c43bce64cc8680a8141d19be7ed
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-01-02 04:59:09 +0530

    Implemented shimmer effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3076824&lng=75.5875939&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
 
     const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
 
-  return (
+  return (listOfRestaurants.length === 0) ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" 
-        onClick={() => {
-          const filteredList = listOfRestaurants.filter(
-            (res) => res.info.avgRating>4.3
-          );
-          setListOfRestaurants(filteredList); 
-        }}
+        <button className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setListOfRestaurants(filteredList);
+          }}
         >
           Top Rated Restaurants</button>
       </div>
