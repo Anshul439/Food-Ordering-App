18f62156d3ebb8541040d45698ec3a6f2ab32156
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-01-03 00:06:49 +0530

    Added login/logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+    console.log("LSLKSJDJ");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -8,10 +13,18 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    <li className="menu-items">Home</li>
+                    <li className="menu-items">About Us</li>
+                    <li className="menu-items">Contact Us</li>
+                    <li className="menu-items">Cart</li>
+                    <li className="login-list"><button className="login"
+                    onClick={() => {
+                        btnNameReact === "Login"
+                        ? setBtnNameReact("Logout")
+                        : setBtnNameReact("Login")
+                    }}>
+                        {btnNameReact}
+                    </button></li>
                 </ul>
             </div>
         </div>
