896259aef4033baa22c42e8561472916e0b96432
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-01-13 20:19:01 +0530

    Improved UI of restaurant categories page

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,22 +1,34 @@
-const ItemList = ({items}) => {
-    console.log(items);
+import { CDN_URL } from "../utils/constants";
+
+const ItemList = ({ items }) => {
+  console.log(items);
   return (
     <div>
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-200 border-b-2 text-left"
+          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
         >
-          <div className="py-2">
-            <span>{item.card.info.name}</span>
-            <span>
-              - ₹
-              {item.card.info.price
-                ? item.card.info.price / 100
-                : item.card.info.defaultPrice / 100}
-            </span>
+          
+          <div className="w-9/12">
+            <div className="py-2">
+              <span>{item.card.info.name}</span>
+              <span>
+                - ₹
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}
+              </span>
+            </div>
+            <p className="text-xs">{item.card.info.description}</p>
           </div>
-          <p className="text-xs">{item.card.info.description}</p>
+          <div className="w-3/12 p-4">
+            
+            <div className="absolute">
+                <button className="p-1 mx-8 my-[86px] rounded-lg bg-white text-green-600 shadow-lg">Add +</button>
+            </div>
+            <img src={CDN_URL + item.card.info.imageId} className="w-28"></img>
+            </div>
         </div>
       ))}
     </div>
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -5,7 +5,7 @@ const RestaurantCategory = ({ data }) => {
   return (
     <div>
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between cursor-pointer">
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
