74713dde60f1bedb03f3d779b88a745da1e74842
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-01-22 20:27:16 +0530

    Created test cases for contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact</h1>
+      <form>
+        <input type="text" className="border border-black p-2 m-2" placeholder="name"/>
+        <input type="text" className="border border-black p-2 m-2" placeholder="message"/>
+        <button className="border border-black p-2 m-2 bg-gray-100 rounded-lg">Submit</button>
+      </form>
     </div>
   );
 };
